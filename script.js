2f3fa999b251f092a5dec24750947ccccc8d30e8
Gee Brain <admin@gozkybrain.com.ng>
2023-01-30 16:25:40 +0100

    initial

--- script.js
+++ script.js
@@ -1,73 +1,74 @@
 var myVar;
 
-        function myFunction() {
-            myVar = setTimeout(showPage, 2000);
-        }
+function myFunction() {
+    myVar = setTimeout(showPage, 2000);
+}
 
-        function showPage() {
-            document.getElementById("loader").style.display = "none";
-            document.getElementById("myDiv").style.display = "block";
-        }
-
-
-        // random value generated
-
-        var y = Math.floor(Math.random() * 10 + 1);
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+}
 
 
+// random value generated
 
-        // counting the number of guesses
-
-        // made for correct Guess
-
-        var guess = 1;
+var y = Math.floor(Math.random() * 10 + 1);
 
 
 
-        document.getElementById("submitguess").onclick = function () {
+// counting the number of guesses
+
+// made for correct Guess
+
+var guess = 1;
 
 
 
-            // number guessed by user
-
-            var x = document.getElementById("guessField").value;
-
-
-            // css for z variale
-
-            var bra = "<div class='brazil'>";
-            var il = "</div>";
+document.getElementById("submitguess").onclick = function () {
 
 
 
+    // number guessed by user
 
-            // results
-
-            var z;
+    var x = document.getElementById("guessField").value;
 
 
-            if (x == y) {
+    // css for z variale
 
-                z = "Wow! You got it!";
+    var bra = "<div class='brazil'>";
+    var il = "</div>";
 
-            }
 
-            else if (x > y) /* if guessed number is greater
+
+
+    // results
+
+    
+    var z;
+
+
+    if (x == y) {
+
+        z = "Wow! You got it!";
+
+    }
+
+    else if (x > y) /* if guessed number is greater
 
                                                    than actual number*/ {
 
-                guess++;
+        guess++;
 
-                z = "OOPS You're wrong, The Answer is " + y + "!";
+        z = "OOPS You're wrong, The Answer is " + y + "!";
 
-            }
+    }
 
-            else {
+    else {
 
-                guess++;
+        guess++;
 
-                z = "OOPS You're wrong, The Answer is " + y + "!";
+        z = "OOPS You're wrong, The Answer is " + y + "!";
 
-            }
-            document.getElementById("demo").innerHTML = bra + z + "<br /><br /><div class=''>" + "<button class='return' onClick='window.location.reload();'>Try Again</button>" + "</div > " + il;
-        }
+    }
+    document.getElementById("demo").innerHTML = bra + z + "<br /><br /><div class=''>" + "<button class='return' onClick='window.location.reload();'>Try Again</button>" + "</div > " + il;
+}
